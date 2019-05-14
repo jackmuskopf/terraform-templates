2490aed68b2448c1dfaf7f46aa8f387edbba8105
Jack Muskopf <jmuskopf@wustl.edu>
2019-05-14 13:18:33 -0500

    lambda use a POST

--- api-gateway-lambda/main.tf
+++ api-gateway-lambda/main.tf
@@ -38,7 +38,7 @@ POLICY
 }
 
 resource "aws_api_gateway_resource" "resource" {
-  path_part   = "resource"
+  path_part   = "my-resource"
   parent_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
 }
@@ -54,7 +54,7 @@ resource "aws_api_gateway_integration" "integration" {
   rest_api_id             = "${aws_api_gateway_rest_api.api.id}"
   resource_id             = "${aws_api_gateway_resource.resource.id}"
   http_method             = "${aws_api_gateway_method.method.http_method}"
-  integration_http_method = "ANY"
+  integration_http_method = "POST"
   type                    = "AWS_PROXY"
 
   # date in uri is api version of lambda?
@@ -117,4 +117,8 @@ resource "aws_iam_role" "role" {
   ]
 }
 POLICY
+}
+
+output "invoke_url" {
+  value = "${aws_api_gateway_deployment.deploy.invoke_url}/${aws_api_gateway_resource.resource.path_part}"
 }
